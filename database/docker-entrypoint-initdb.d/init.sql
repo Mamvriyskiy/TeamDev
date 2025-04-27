b77d541ca9071f4d87c6b7ed2741408e4bc052cb
imamvriyskiy <imamvriyskiy@ivi.ru>
2025-04-27 23:27:25 +0300

    Обновил БД

--- database/docker-entrypoint-initdb.d/init.sql
+++ database/docker-entrypoint-initdb.d/init.sql
@@ -1,63 +1,51 @@
--- Создание схемы warehouse для history_item
-CREATE SCHEMA IF NOT EXISTS warehouse;
-
--- Таблица warehouse.history_item
-CREATE TABLE warehouse.history_item (
-    id SERIAL PRIMARY KEY,
-    action TEXT NOT NULL,
-    item_type TEXT NOT NULL,
-    item_id INTEGER NOT NULL,
-    item JSONB NOT NULL
-);
-    
 -- Таблица data.user
 CREATE TABLE user_account (
-    id SERIAL PRIMARY KEY,
-    telegram nvarchar(32),
-    balance int,
-    blocked bit
+    id UUID PRIMARY KEY,
+    telegram VARCHAR(32),
+    balance INT,
+    blocked BOOLEAN  -- Изменено на BOOLEAN
 );
 
 -- Таблица data.account
 CREATE TABLE account (
-    id SERIAL PRIMARY KEY,
-    owner_id uniqueidentifier,
-    social_network nvarchar(max),
-    url nvarchar(max),
+    id UUID PRIMARY KEY,
+    owner_id UUID,
+    social_network VARCHAR(255),
+    url VARCHAR(255),
     FOREIGN KEY (owner_id) REFERENCES user_account(id)
 );
 
 -- Таблица data.request
 CREATE TABLE request (
-    id SERIAL PRIMARY KEY,
-    account_id uniqueidentifier,
+    id UUID PRIMARY KEY,
+    account_id UUID,
     requested_followers INTEGER NOT NULL,
     requested_duration INTEGER NOT NULL,
     prompt INTEGER NOT NULL,
-    follow_rewardINTEGER NOT NULL,
+    follow_reward INTEGER NOT NULL,  -- Исправлено: добавлен пробел
     cost_per_follower INTEGER NOT NULL,
-    status nvarchar(max),
-    last_changed_at datetime(2)	,
+    status TEXT,  -- Изменено на TEXT
+    last_changed_at TIMESTAMP,  -- Изменено на TIMESTAMP
     FOREIGN KEY (account_id) REFERENCES account(id)
 );
 
 -- Таблица data.offer
 CREATE TABLE offer (
-    id SERIAL PRIMARY KEY,
-    recipient_id uniqueidentifier,
-    request_id uniqueidentifier,
-    status nvarchar(max),
-    last_changed_at datetime(2),
+    id UUID PRIMARY KEY,
+    recipient_id UUID,
+    request_id UUID,
+    status TEXT,  -- Изменено на TEXT
+    last_changed_at TIMESTAMP,  -- Изменено на TIMESTAMP
     FOREIGN KEY (recipient_id) REFERENCES user_account(id),
     FOREIGN KEY (request_id) REFERENCES request(id)
 );
 
 -- Таблица data.subscription
 CREATE TABLE subscription (
-    id SERIAL PRIMARY KEY,
-    offer_id uniqueidentifier,
-    followee_id uniqueidentifier,
-    last_verified_at datetime(2),
+    id UUID PRIMARY KEY,
+    offer_id UUID,
+    followee_id UUID,
+    last_verified_at TIMESTAMP,  -- Изменено на TIMESTAMP
     FOREIGN KEY (offer_id) REFERENCES offer(id),
     FOREIGN KEY (followee_id) REFERENCES user_account(id)
 );
